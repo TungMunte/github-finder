0cf1501d17d58f1f110629db71b93df79cdaf119
Tung NGUYEN QUANG 322CD <seeyouagain14012000@gmail.com>
2022-11-25 19:38:19 +0200

    first commit

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { createContext, useReducer } from "react";
+import GithubReducer from "./GithubReducer";
+
+const GithubContext = createContext();
+
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: true,
+  };
+
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+  return (
+    <GithubContext.Provider
+      value={{
+        ...state,
+        dispatch,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext;
